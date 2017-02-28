abe67a3c20a38e34d91f961cbd086cbcbc4757af
hananana <hananana.vimmer@gmail.com>
2017-02-28 09:52:22 +0900

    :new: available pip install

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
 # coding: UTF-8
+
+from setuptools import setup
+
+setup(
+    name='iris',
+    version='1.0',
+    packages=['iris'],
+    description='export iOS/Android project from unity in cli',
+    author='hananana',
+    license='MIT',
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        iris=iris:main
+    ''',
+)
